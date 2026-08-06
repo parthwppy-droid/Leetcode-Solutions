0ec867caabbf91e2110654e36eba92ceda354a8f
parthwppy-droid <parthwppy@gmail.com>
2026-08-06 17:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (59.82%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void backtrack(int index, vector<int>& candidates, int target,
+                   vector<int>& current, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        if (target < 0 || index == candidates.size()) {
+            return; // dead end, backtrack
+        }
+
+        current.push_back(candidates[index]);
+        backtrack(index, candidates, target - candidates[index], current,
+                  result);
+        current.pop_back();
+
+        backtrack(index + 1, candidates, target, current, result);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> current;
+        vector<vector<int>> result;
+        backtrack(0, candidates, target, current, result);
+        return result;
+    }
+};
