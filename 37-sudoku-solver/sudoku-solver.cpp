4fa055f3005aea5a220d6f05f149eb33850e260a
parthwppy-droid <parthwppy@gmail.com>
2026-08-07 16:59:12 +0530

    Time: 384 ms (31.70%) | Memory: 8.8 MB (40.50%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c)
+                return false; 
+            if (board[i][col] == c)
+                return false; 
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == c)
+                return false; 
+        }
+        return true;
+    }
+
+    bool backtrack(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] != '.')
+                    continue; 
+
+                for (char c = '1'; c <= '9'; c++) {
+                    if (isValid(board, row, col, c)) {
+                        board[row][col] = c; 
+                        if (backtrack(board))
+                            return true; 
+                                         
+                        board[row][col] = '.'; 
+                    }
+                }
+                return false; 
+            }
+        }
+        return true; 
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        backtrack(board); 
+    }
+};
